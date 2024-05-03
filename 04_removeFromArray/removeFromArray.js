a3ee34e70b3f5c1f76095e90ab96f1d8895c2d6b
DoMa157 <82661850+DoMa157@users.noreply.github.com>
2024-05-03 23:10:07 +0400

    Add solution to javascript exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    let resArr = []
+    for(let i = 0; i<arr.length; i++){
+        if(args.includes(arr[i])){
+            continue;
+        }
+        resArr.push(arr[i]);
+    }
+    return resArr;
 };
 
 // Do not edit below this line
