aca2d494eba73cf4786b65e1a10ed61fe7842f09
DoMa157 <82661850+DoMa157@users.noreply.github.com>
2024-05-03 23:15:15 +0400

    Add solution to javascript exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(lowerLimit, upperLimit) {
+    if(lowerLimit < 0 || upperLimit < 0 || typeof(lowerLimit) != "number" || typeof(upperLimit) != "number"){
+        return 'ERROR';
+    }
+    if(upperLimit < lowerLimit){
+        let temp = upperLimit;
+        upperLimit = lowerLimit;
+        lowerLimit = temp;
+    }
+    let sum = 0;
+    for(let i = lowerLimit; i<=upperLimit; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
