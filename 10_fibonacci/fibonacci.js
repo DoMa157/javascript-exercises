2164bc9c9ab773508c0e4bcbac569941cdfed616
DoMa157 <82661850+DoMa157@users.noreply.github.com>
2024-05-05 00:25:18 +0400

    Add solution to js exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let dp = [1, 1];
+    if(num < 0){
+        return "OOPS";
+    }
+    if(num == 0){
+        return 0;
+    }
+    if(num < 2){
+        return dp[num-1];
+    }
+    for(let i = 2; i<=parseInt(num); i++){
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+    return dp[num-1];
 };
 
 // Do not edit below this line
