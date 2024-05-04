099a9687d316822961e8c321340116116473091b
DoMa157 <82661850+DoMa157@users.noreply.github.com>
2024-05-05 00:18:17 +0400

    Add solution to js exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    arr = Array.from(word.replace(/[^a-zA-Z0-9]/g, ''));
+    for(let i = 0, j = arr.length - 1; i<arr.length && j>=0; i++, j--){
+        if(arr[i].toLowerCase() != arr[j].toLowerCase()){
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
