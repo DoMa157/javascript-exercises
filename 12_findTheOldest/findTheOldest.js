f32d285df12702893deebe87b282024fe8675421
DoMa157 <82661850+DoMa157@users.noreply.github.com>
2024-05-05 00:35:33 +0400

    Add solution to js exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people.forEach(element => {
+        if(element.yearOfDeath == undefined){
+            element.yearOfDeath = 2024;
+        }
+    });
+    people.sort((x,y) => x.yearOfDeath - x.yearOfBirth < y.yearOfDeath - y.yearOfBirth ? 1 : -1);
+    return people[0];
 };
 
 // Do not edit below this line
